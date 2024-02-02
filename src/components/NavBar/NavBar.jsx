1af9e92ea0dd1d49af011f76c5478d47a03e1b31
Nate Sobeck <nsobeck910@gmail.com>
2024-02-02 11:29:01 -0500

    choose font styling and add header to main page

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,24 +3,27 @@ import { NavLink } from 'react-router-dom'
 
 const NavBar = ({ user, handleLogout }) => {
   return (
-    <nav className="main-nav">
-      {user ?
-        <ul>
-          <li>Welcome, {user.name}</li>
-          <li><NavLink to="/">Home</NavLink></li>
-          <li><NavLink to="/profiles">Profiles</NavLink></li>
-          <li><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></li>
-          <li><NavLink to="/auth/change-password">Change Password</NavLink></li>
-          <li><NavLink to="/trips">All Trips</NavLink></li>
-        </ul>
-      :
-        <ul>
-          <li><NavLink to="/">Home</NavLink></li>
-          <li><NavLink to="/auth/login">Log In</NavLink></li>
-          <li><NavLink to="/auth/signup">Sign Up</NavLink></li>
-        </ul>
-      }
-    </nav>
+    <>
+      <nav className="main-nav">
+        {user ?
+          <ul>
+            <li>Welcome, {user.name}</li>
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/profiles">Profiles</NavLink></li>
+            <li><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></li>
+            <li><NavLink to="/auth/change-password">Change Password</NavLink></li>
+            <li><NavLink to="/trips">All Trips</NavLink></li>
+          </ul>
+        :
+          <ul>
+            <li><NavLink to="/">HOME</NavLink></li>
+            <li><NavLink to="/auth/login">LOG IN</NavLink></li>
+            <li><NavLink to="/auth/signup">SIGN UP</NavLink></li>
+          </ul>
+        }
+      </nav>
+      <h1 className="main-title">Waypoint</h1>
+    </>
   )
 }
 

d959ea2d74f3b6e0712646dba235d32692f1c8f6
Nate Sobeck <nsobeck910@gmail.com>
2024-02-08 13:32:40 -0500

    add styling to schedule item

--- src/components/ScheduleItem/ScheduleItem.jsx
+++ src/components/ScheduleItem/ScheduleItem.jsx
@@ -4,12 +4,16 @@ import styles from './ScheduleItem.module.css'
 const ScheduleItem = ({ scheduleItem }) => {
   return (  
     <div className={styles.container}>
-      <h4>{scheduleItem.name}</h4>
-      <p>{new Date(scheduleItem.startTime).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })} - {new Date(scheduleItem.endTime).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}</p>
-      <p>{scheduleItem.location}</p>
-      <p>{scheduleItem.category}</p>
-      <div className={styles['address-container']}>
-        {scheduleItem.address.street.length ? `${scheduleItem.address.street}, ${scheduleItem.address.city}, ${scheduleItem.address.zipCode}` : ''}
+      <div>
+        <p className={styles.time}>{new Date(scheduleItem.startTime).toLocaleTimeString([], { hour: "numeric", minute: "2-digit" })} - {new Date(scheduleItem.endTime).toLocaleTimeString([], { hour: "numeric", minute: "2-digit" })}</p>
+      </div>
+      <div className={styles['schedule-content']}>
+        <h4 className={styles.name}>{scheduleItem.name}</h4>
+        <p>{scheduleItem.location}</p>
+        <p>{scheduleItem.category}</p>
+        <div className={styles['address-container']}>
+          {scheduleItem.address.street.length ? `${scheduleItem.address.street}, ${scheduleItem.address.city}, ${scheduleItem.address.zipCode}` : ''}
+        </div>
       </div>
     </div>
   )
